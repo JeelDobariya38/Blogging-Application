96af38a342b826864576d25ccd8f4cbd57ff9ee2
Jeel Dobariya <136002730+JeelDobariya38@users.noreply.github.com>
2024-02-22 11:23:19 +0000

    Add ejs library to serve static files

--- index.js
+++ index.js
@@ -6,8 +6,9 @@ const path = require("path");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.sendfile(path.join(__dirname, "views", "index.html"));
-});
+app.set("view engine", "ejs");
+
+app.use(express.static("views"));
+app.use(express.json());
 
 app.listen(port, () => console.log(`server is running on port ${port}`));
