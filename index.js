6bd04060ebaf60d2ba130bde4f9a70de6cec5d4f
Jeel Dobariya <136002730+JeelDobariya38@users.noreply.github.com>
2024-02-25 06:35:07 +0000

    add a auth router

--- index.js
+++ index.js
@@ -3,6 +3,8 @@
 const express = require("express");
 const path = require("path");
 
+const auth = require("./routers/Auth");
+
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -10,5 +12,14 @@ app.set("view engine", "ejs");
 
 app.use(express.static("public"));
 app.use(express.json());
+
+app.get("api/info", (req, res) => {
+  res.json({
+    message: "Welcome to the Blogging Rest API!!",
+    success: true,
+  });
+});
+
+app.use("api/auth", auth);
 
 app.listen(port, () => console.log(`server is running on port ${port}`));
---
+++ routers/Auth.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const router = express.Router();
+
+const operation = require("../modules/operation");
+
+router.post("/signup", (req, res) => {
+  let { fullname, username, password } = req.body;
+
+  if (!fullname || !username || !password) {
+    return res.status(400).json({ 
+      message: "Please provide all the required fields!!!",
+      success: false
+    });
+  }
+
+  let response= operation.signup(fullname, username, password);
+  return res.status(response.status_code).json(response);
+});
+
+router.post("/login", (req, res) => {
+  let { username, password } = req.body;
+
+  if (!username || !password) {
+    return res.status(400).json({ 
+      message: "Please provide all the required fields!!!",
+      success: false
+    });
+  }
+
+  let response= operation.signup(fullname, username, password);
+  return res.status(response.status_code).json(response);
+});
+
+module.exports = router;
