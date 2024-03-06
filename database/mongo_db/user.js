74c5be32c624c222e491121a8f867d0f37e54d67
Jeel Dobariya <jeeldobariya38@gmail.com>
2024-03-06 19:36:03 +0530

    add user interface

---
+++ database/mongo_db/user.js
@@ -0,0 +1,89 @@
+const User = require("./models/User");
+
+
+const getUserById = (id) => {
+    return new Promise((resolve, reject) => {
+        User.findOne({ _id: id })
+            .then((user) => {
+                resolve(user);
+            })
+            .catch((error) => {
+                reject(error);
+            });
+    });
+};
+
+
+const getUserByUsername = (username) => {
+    return new Promise((resolve, reject) => {
+        User.findOne({ username })
+            .then((user) => {
+                resolve(user);
+            })
+            .catch((error) => {
+                reject(error);
+            });
+    });
+};
+
+
+const createUser = (fullname, username, password) => {
+    return new Promise((resolve, reject) => {
+        User.create({ fullname, username, password })
+            .then((newUser) => {
+                resolve(newUser);
+            })
+            .catch((error) => {
+                reject(error);
+            });
+    });
+};
+
+
+const changeUserPassword = (username, newPassword) => {
+    return new Promise((resolve, reject) => {
+        User.updateOne({ username }, { password: newPassword })
+            .then((result) => {
+                resolve(result);
+            })
+            .catch((error) => {
+                reject(error);
+            });
+    });
+};
+
+
+const deleteUserById = (id) => {
+    return new Promise((resolve, reject) => {
+        User.deleteOne({ _id: id })
+            .then((result) => {
+                resolve(result);
+            })
+            .catch((error) => {
+                reject(error);
+            });
+    });
+};
+
+
+const deleteUserByUsername = (username) => {
+    return new Promise((resolve, reject) => {
+        User.deleteOne({ username })
+            .then((result) => {
+                resolve(result);
+            })
+            .catch((error) => {
+                reject(error);
+            });
+    });
+};
+
+
+module.exports = {
+    getUserById,
+    getUserByUsername,
+    createUser,
+    changeUserPassword,
+    deleteUserById,
+    deleteUserByUsername
+};
