cce5d9985b6ec52ccf722cdda6d80ecf45ac8c1e
Jeel Dobariya <jeeldobariya38@gmail.com>
2024-03-08 21:07:19 +0530

    add timestamps to database (mongodb)
    this is only done to mongodb database and not to local database

--- database/mongo_db/models/Blog.js
+++ database/mongo_db/models/Blog.js
@@ -1,22 +1,27 @@
 const mongoose = require('mongoose');
 
 
-const blogSchema = new mongoose.Schema({
-    title: {
-        type: String,
-        unique: true,
-        required: true
+const blogSchema = new mongoose.Schema(
+    {
+        title: {
+            type: String,
+            unique: true,
+            required: true
+        },
+        content: {
+            type: String,
+            required: true
+        },
+        author: {
+            type: mongoose.Types.ObjectId,
+            ref: "User",
+            required: true
+        },
     },
-    content: {
-        type: String,
-        required: true
-    },
-    author: {
-        type: mongoose.Types.ObjectId,
-        ref: "User",
-        required: true
-    },
-});
+    {
+        timestamps: true
+    }
+);
 
 
 module.exports = mongoose.model('Blog', blogSchema);
--- database/mongo_db/models/User.js
+++ database/mongo_db/models/User.js
@@ -1,21 +1,26 @@
 const mongoose = require('mongoose');
 
 
-const userSchema = new mongoose.Schema({
-    fullname: {
-        type: String,
-        required: true
+const userSchema = new mongoose.Schema(
+    {
+        fullname: {
+            type: String,
+            required: true
+        },
+        username: {
+            type: String,
+            unique: true,
+            required: true
+        },
+        password: {
+            type: String,
+            required: true
+        },
     },
-    username: {
-        type: String,
-        unique: true,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-});
+    {
+        timestamps: true
+    }
+);
 
 
 module.exports = mongoose.model('User', userSchema);
